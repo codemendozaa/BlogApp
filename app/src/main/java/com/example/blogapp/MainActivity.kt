3935920793ca0aa071a13bdae3a93560fee5ab67
erixjavier <Mendhoza123#>
2022-03-28 18:29:05 -0500

    example the take picture in storage the firebase & crashlytics

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -1,54 +1,84 @@
 package com.example.blogapp
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val db = FirebaseFirestore.getInstance()
-
-        /*//consultar informacion de un solo document
-        
-        db.collection("ciudades").document("LA").get().addOnSuccessListener { documet ->
-            documet?.let {
-                Log.d("Firebase", "documentSnapshop data: ${documet.data}")
-                val ciudad = documet.toObject(Ciudad::class.java)
-
-                Log.d("Firebase", "population: ${ciudad?.population}")
-                Log.d("Firebase", "color: ${ciudad?.color}")
-            }
-
-        }.addOnFailureListener { error ->
-            Log.e("FirebaseError", error.toString())
-        }*/
-        
-        //consultar y ver en tiempo real cambios 
-        db.collection("ciudades").document("LA").addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
-            documentSnapshot?.let { document->
-                Log.d("Firebase", "documentSnapshop data: ${document.data}")
-                val ciudad = document.toObject(Ciudad::class.java)
-
-                Log.d("Firebase", "population: ${ciudad?.population}")
-                Log.d("Firebase", "color: ${ciudad?.color}")
-
-
-            }
-        }
-
-        //Ingresar Informacion
-        db.collection("ciudades").document("NY").set(Ciudad(300000,"red")).addOnSuccessListener {
-            Log.d("Firebase", "se guardo correctamente la ciudad")
-        }.addOnFailureListener { error->
-            Log.e("FirebaseError", error.toString())
+        val btnTakePicture = findViewById<Button>(R.id.btn_take_picture)
+        imageView = findViewById(R.id.imageView)
+        btnTakePicture.setOnClickListener {
+            dispatchTakePictureIntent()
         }
     }
-}
 
-    data class  Ciudad(val population:Int = 0,
-                       val color:String ="")
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                applicationContext,
+                "No se encontro app para tomar la foto",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+            uploadPicture(imageBitmap)
+        }
+    }
+
+    private fun uploadPicture(bitmap: Bitmap) {
+        val storaRef = FirebaseStorage.getInstance().reference
+        val imageRef = storaRef.child("imagenes/${UUID.randomUUID()}.jpg")
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let { exception ->
+                    throw exception
+                }
+            }
+            imageRef.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val downloadUrl = task.result.toString()
+                FirebaseFirestore.getInstance().collection("ciudades").document("LA").update(mapOf("imageUrl" to downloadUrl))
+                Log.d("Storage", "uploadPictureURL: $downloadUrl")
+            }
+
+        }
+    }
+
+}
