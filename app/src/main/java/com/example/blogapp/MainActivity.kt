a3fc9dc5c61ca80bad86b712bde498b2287ecd31
erixjavier <Mendhoza123#>
2022-04-07 17:24:13 -0500

    Creando menu de navegacion botom

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -3,53 +3,22 @@ package com.example.blogapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.blogapp.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater    )
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
-        val db = FirebaseFirestore.getInstance()
-
-        /*//consultar informacion de un solo document
-        
-        db.collection("ciudades").document("LA").get().addOnSuccessListener { documet ->
-            documet?.let {
-                Log.d("Firebase", "documentSnapshop data: ${documet.data}")
-                val ciudad = documet.toObject(Ciudad::class.java)
-
-                Log.d("Firebase", "population: ${ciudad?.population}")
-                Log.d("Firebase", "color: ${ciudad?.color}")
-            }
-
-        }.addOnFailureListener { error ->
-            Log.e("FirebaseError", error.toString())
-        }
-        
-        //consultar y ver en tiempo real cambios 
-        db.collection("ciudades").document("LA").addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
-            documentSnapshot?.let { document->
-                Log.d("Firebase", "documentSnapshop data: ${document.data}")
-                val ciudad = document.toObject(Ciudad::class.java)
-
-                Log.d("Firebase", "population: ${ciudad?.population}")
-                Log.d("Firebase", "color: ${ciudad?.color}")
-
-
-            }
-        }
-
-        //Ingresar Informacion
-        db.collection("ciudades").document("NY").set(Ciudad(300000,"red")).addOnSuccessListener {
-            Log.d("Firebase", "se guardo correctamente la ciudad")
-        }.addOnFailureListener { error->
-            Log.e("FirebaseError", error.toString())
-        }*/
     }
 }
-/*
-
-    data class  Ciudad(val population:Int = 0,
-                       val color:String ="")*/
