bee1bd46bbf0ef77a32ed1466d46eff74bee9f73
erixjavier <Mendhoza123#>
2022-04-12 09:28:12 -0500

    Fixeando bug popup a login desde setupProfile

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -41,6 +41,10 @@ class MainActivity : AppCompatActivity() {
                     binding.bottomNavigationView.hide()
                 }
 
+                R.id.setupProfileFragment ->{
+                    binding.bottomNavigationView.hide()
+                }
+
                 else -> {
                     binding.bottomNavigationView.show()
                 }
