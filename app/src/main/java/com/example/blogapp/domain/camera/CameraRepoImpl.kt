d3688f9ebf8c50a9840149fc69aff29380251654
erixjavier <Mendhoza123#>
2022-04-12 10:58:44 -0500

    Creando logica para subir post a firebase

---
+++ app/src/main/java/com/example/blogapp/domain/camera/CameraRepoImpl.kt
@@ -0,0 +1,10 @@
+package com.example.blogapp.domain.camera
+
+import android.graphics.Bitmap
+
+class CameraRepoImpl: CameraRepo {
+    override suspend fun uploadPhoto(imageBitmap: Bitmap, description: String) {
+        TODO("Not yet implemented")
+    }
+
+}
