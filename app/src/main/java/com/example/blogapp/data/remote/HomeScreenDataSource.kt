7491eb53d66ba21473a383bdb9a1360e7a1cb507
erixjavier <Mendhoza123#>
2022-04-13 10:09:15 -0500

    Ordenar posts por fecha de creación

--- app/src/main/java/com/example/blogapp/data/remote/HomeScreenDataSource.kt
+++ app/src/main/java/com/example/blogapp/data/remote/HomeScreenDataSource.kt
@@ -4,12 +4,13 @@ import com.example.blogapp.core.Result
 import com.example.blogapp.data.model.Post
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 class HomeScreenDataSource {
     suspend fun getLatesPosts(): Result<List<Post>> {
         val postList  = mutableListOf<Post>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("post").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("post").orderBy("created_at",Query.Direction.DESCENDING).get().await()
         for (post in querySnapshot.documents){
             post.toObject(Post::class.java )?.let { fbPost ->
                 fbPost.apply {
