d7f352fcb3c7f0b9f302b2e96471f8f77c5c6d81
erixjavier <Mendhoza123#>
2022-04-13 15:12:30 -0500

    Creando likes en nuestro post

--- app/src/main/java/com/example/blogapp/data/model/Post.kt
+++ app/src/main/java/com/example/blogapp/data/model/Post.kt
@@ -1,13 +1,21 @@
 package com.example.blogapp.data.model
 
+import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
 
-data class Post(val profile_picture:String="",
-                val profile_name:String="",
+data class Post(
+                @Exclude @JvmField
+                val id:String ="",
                 @ServerTimestamp
                 var created_at: Date?= null,
                 val post_image:String = "",
                 val post_description:String = "",
-                val uid:String = "")
+                val poster:Poster? = null,
+                val like:Long = 0,
+                @Exclude
+                @JvmField
+                val liked:Boolean  = false)
+
+data class Poster(val username:String? ="",val uid:String? ="",val profile_picture:String="")
